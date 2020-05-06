4007e8ede658feaa720c27797150da60453c20ac
Jesse Walton <Jesse.M.Walton@gmail.com>
2020-05-05 21:19:26 -0400

    Add full test coverage

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,62 +1,181 @@
+// all items 
+// - sellIn: days to sell the item
+// - quality: denotes value of item
+
+/*
+  sellBy > 0, quality -= 1 per day
+  sellby = 0, quality -= 2 per day
+
+  quality >= 0 && quality <= 50
+
+  if (agedBrie) quality += 1
+  if (sulfuras) never has to be sold or loses quality
+  if (backstagePasses) {
+    if (sellIn > 10) quality -= 1
+    elif (sellIn <= 10) quality += 2
+    elif (sellIn <= 5) quality += 3
+    else { quality = 0 }
+
+  }
+
+
+
+
+
+*/
+
+
+
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+    this.sellIn = sellIn; // days to sell the item (lower daily)
+    this.quality = quality; // value of item (lower daily)
   }
 }
+
+/*
+
+other 
+  sellIn -= 1
+  if (quality > 0) quality -= 1
+  if (quality > 0 && sellIn < 0) quality -= 1
+
+aged brie 
+  sellIn -= 1
+  if (quality < 50) quality += 1
+  if (sellIn < 0 && quality < 50) quality += 1
+
+backstage passes
+  sellIn -= 1
+  if (sellIn > 10) quality += 1
+    elif (sellIn <= 10) quality += 2
+    elif (sellIn <= 5) quality += 3
+    elif (sellIn < 0) quality = 0 
+
+sulfuras
+
+
+
+((if it's not 'aged brie' AND it's not 'backstage passes') and quality is positive) and it is not sulfuras -> reduce quality by 1
+if not 'aged brie' or 'backstage passes' and quality is positive 
+(if it is 'aged brie' or 'backstage passes') and quality is less than 50 -> increase quality by 1
+((((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 11) and quality is less than 50 -> increase quality by 1
+
+(if it is not 'sulfuras') -> reduce sellIn by 1
+((((if sellIn is less than 0) and it is not 'aged brie') and it is not 'backstage passes') and quality is greater than 0) and it is not 'sulfuras' -> reduce quality by 1
+((if sellIn is less than 0) and it is not 'aged brie') and it is 'backstage passes' -> set quality to 0
+((if sellIn is less than 0) and it is 'aged brie') and quality is less than 50 -> increase quality by 1
+*/
 
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
+
+    // for each item
     for (let i = 0; i < this.items.length; i++) {
+
+      // if it's not 'aged brie' AND it's not 'backstage passes'
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+
+        // (if it's not 'aged brie' AND it's not 'backstage passes') and quality is positive
         if (this.items[i].quality > 0) {
+
+          // ((if it's not 'aged brie' AND it's not 'backstage passes') and quality is positive) and it is not sulfuras
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+
+            // reduce quality by 1
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
+      } 
+      // if it is 'aged brie' or 'backstage passes'
+      else {
+
+        // (if it is 'aged brie' or 'backstage passes') and quality is less than 50
         if (this.items[i].quality < 50) {
+
+          // increase quality by 1
           this.items[i].quality = this.items[i].quality + 1;
+
+          // ((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes'
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+
+            // (((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 11
             if (this.items[i].sellIn < 11) {
+              
+              // ((((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 11) and quality is less than 50
               if (this.items[i].quality < 50) {
+
+                // increase quality by 1
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+
+            // (((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 6
             if (this.items[i].sellIn < 6) {
+
+              // ((((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 6) and quality is less than 50
               if (this.items[i].quality < 50) {
+
+                // increase quality by 1
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
         }
       }
+
+      // if it is not 'sulfuras'
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+
+        // reduce sellIn by 1
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
+      // if sellIn is less than 0
       if (this.items[i].sellIn < 0) {
+
+        // (if sellIn is less than 0) and it is not 'aged brie'
         if (this.items[i].name != 'Aged Brie') {
+
+          // ((if sellIn is less than 0) and it is not 'aged brie') and it is not 'backstage passes'
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+
+            // (((if sellIn is less than 0) and it is not 'aged brie') and it is not 'backstage passes') and quality is greater than 0
             if (this.items[i].quality > 0) {
+
+              // ((((if sellIn is less than 0) and it is not 'aged brie') and it is not 'backstage passes') and quality is greater than 0) and it is not 'sulfuras'
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+
+                // reduce quality by 1
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
+          } 
+
+          // ((if sellIn is less than 0) and it is not 'aged brie') and it is 'backstage passes'
+          else {
+
+            // set quality to 0
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
+        } 
+        
+        // (if sellIn is less than 0) and it is 'aged brie'
+        else {
+
+          // ((if sellIn is less than 0) and it is 'aged brie') and quality is less than 50
           if (this.items[i].quality < 50) {
+
+            // increase quality by 1
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
     }
-
     return this.items;
   }
 }
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,168 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+
+  describe("other items", function() {
+    it("should decrement expiration date", function() {
+      const gildedRose = new Shop([new Item("", 25, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(24);
+    });
+
+    it("should decrement expiration date past 0", function() {
+      const gildedRose = new Shop([new Item("", 0, 50)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    it("should degrade quality", function() {
+      const gildedRose = new Shop([new Item("", 50, 25)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(24);
+    });
+
+    it("should degrade quality twice as fast if past expiration date", function() {
+      const gildedRose = new Shop([new Item("", 0, 25)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(23);
+    });
+
+    it("should not degrade quality below 0", function() {
+      const gildedRose = new Shop([new Item("", 0, 1)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+  });
+
+  describe("Aged Brie", function() {
+    it("should decrement expiration date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 25, 25)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(24);
+    });
+
+    it("should decrement expiration date past 0", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 0, 1)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    it("should increase in quality by 1 if before expiration date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 50, 0)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+    });
+    
+    it("should increase in quality twice as fast if past expiration date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(2);
+    });
+
+    it("should limit quality to 50 if before expiration date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 50, 50)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    });
+
+    it("should limit quality to 50 if past expiration date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 0, 49)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    });
+  });
+
+  describe("backstage passes", function() {
+    it("should decrement expiration date", function() {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 25, 25)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(24);
+    });
+
+    it("should decrement expiration date past 0", function() {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 1)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+    });
+
+    it("should increase in quality by 1 if more than 10 days before expiration date", function() {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(1);
+    });
+
+    it("should increase quality by 2 if between 6 and 10 days before expiration date", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 6, 0),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(2);
+      expect(items[1].quality).toBe(2);
+    });
+
+    it("should increase quality by 3 if between 1 and 5 days before expiration date", function() {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 1, 0),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(3);
+      expect(items[1].quality).toBe(3);
+    });
+
+    it("should set quality to 0 if past expiration date", function() {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+  });
+
+  describe("sulfuras", function() {
+    it("should not expire", function() {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 25, 25)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(25);
+    });
+
+    it("should not lose value", function() {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 25, 25)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(25);
+    });
   });
 });
+
+
+/*
+
+other 
+  sellIn -= 1
+  if (quality > 0) quality -= 1
+  if (quality > 0 && sellIn < 0) quality -= 1
+
+aged brie 
+  sellIn -= 1
+  if (quality < 50) quality += 1
+  if (sellIn < 0 && quality < 50) quality += 1
+
+backstage passes
+  sellIn -= 1
+  if (sellIn > 10) quality += 1
+    elif (sellIn <= 10) quality += 2
+    elif (sellIn <= 5) quality += 3
+    elif (sellIn < 0) quality = 0 
+
+sulfuras
+
+boolean logic notes:
+((if it's not 'aged brie' AND it's not 'backstage passes') and quality is positive) and it is not sulfuras -> reduce quality by 1
+if not 'aged brie' or 'backstage passes' and quality is positive 
+(if it is 'aged brie' or 'backstage passes') and quality is less than 50 -> increase quality by 1
+((((if it is 'aged brie' or 'backstage passes') and quality is less than 50) and it is 'backstage passes') and sellIn is less than 11) and quality is less than 50 -> increase quality by 1
+
+(if it is not 'sulfuras') -> reduce sellIn by 1
+((((if sellIn is less than 0) and it is not 'aged brie') and it is not 'backstage passes') and quality is greater than 0) and it is not 'sulfuras' -> reduce quality by 1
+((if sellIn is less than 0) and it is not 'aged brie') and it is 'backstage passes' -> set quality to 0
+((if sellIn is less than 0) and it is 'aged brie') and quality is less than 50 -> increase quality by 1
+*/
